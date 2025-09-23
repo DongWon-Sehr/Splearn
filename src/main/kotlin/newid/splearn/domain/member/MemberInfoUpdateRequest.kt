00d15ae1de4a4335f68592e30f57e52c775ef8d6
DongWon-Sehr <ddoro36@gmail.com>
2025-09-23 13:12:26 +0900

    :memo: update document

--- src/main/kotlin/newid/splearn/domain/member/MemberInfoUpdateRequest.kt
+++ src/main/kotlin/newid/splearn/domain/member/MemberInfoUpdateRequest.kt
@@ -1,9 +1,10 @@
 package newid.splearn.domain.member
 
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class MemberInfoUpdateRequest(
     @field:Size(min = 5, max = 15) val nickname: String,
     @field:Size(min = 1, max = 15) val profileAddress: String,
-    val introduction: String,
+    @NotNull val introduction: String,
 )
